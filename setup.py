4429a733c6f5e91276fbef7f1cb2f4db6d7d3bd7
Dirk Groeneveld <groeneveld@gmail.com>
2021-11-17 23:32:17 -0800

    Get ready to release v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='bettermap',
-  version='1.0.0',
+  version='1.1.0',
   description="Drop-in replacements for Python's map function",
   url='https://github.com/allenai/bettermap',
   author="Dirk Groeneveld",
